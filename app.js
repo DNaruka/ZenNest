29e216c987da106a604cfd6ca3d8d480c125e252
Diskha Naruka <dikshareptime@gmail.com>
2024-06-23 18:54:15 -0400

    Server Boilerplate added

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendStatus(200);
+})
+
+app.listen(3000, () => {
+    console.log('Server running at Port: '+ 3000);
+})
