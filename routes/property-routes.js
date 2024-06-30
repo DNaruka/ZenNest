17c75170d79b8787d1cf7e283d62f6962218f970
Diskha Naruka <dikshareptime@gmail.com>
2024-06-30 17:50:44 -0400

    Add get Property

--- routes/property-routes.js
+++ routes/property-routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getProperties } from "../controllers/property-controller.js";
+import { getProperties, getProperty } from "../controllers/property-controller.js";
 import auth from "../auth.js";
 
 const router = Router();
 router.use(auth);
 router.route("/").get(getProperties);
+
+router.route("/:propertyId").get(getProperty);
 
 export default router;
