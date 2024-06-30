a6f4672868589bb0f5a444692d80cf76532567cc
Diskha Naruka <dikshareptime@gmail.com>
2024-06-30 17:50:44 -0400

    Add get Property

--- controllers/property-controller.js
+++ controllers/property-controller.js
@@ -7,7 +7,6 @@ const knex = _knex(knexfile);
 
 export const getProperties = async (req, res) => {
   const userData = await decryptAndVerifyToken(req);
-  console.log(userData);
 
   const data = await knex("property")
     .innerJoin(
@@ -18,4 +17,31 @@ export const getProperties = async (req, res) => {
     .where("property_admin.admin_id", userData.adminId);
 
   res.status(200).send(data);
+};
+
+export const getProperty = async (req, res) => {
+  const propertyId = req.params.propertyId;
+
+  try {
+    if (!Number.isInteger(+propertyId)) throw new Error("Invalid Property Id");
+
+    const userData = await decryptAndVerifyToken(req);
+
+    const data = await knex("property")
+      .innerJoin(
+        "property_admin",
+        "property.property_id",
+        "property_admin.property_id"
+      )
+      .where("property_admin.admin_id", userData.adminId)
+      .where("property.property_id", propertyId);
+
+    if (data.length === 0) {
+      return res.status(401).send("Unauthorized to view property");
+    }
+
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
 };
